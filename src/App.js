182e8a26df4d7ac3ac0e18a13b25f7696e3b50ff
sanjaydambal <sanjaydambal@gmail.com>
2024-05-03 17:28:28 +0530

    created cartcontext and provided routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Checkout from './Components/Checkout/Checkout';
+ import NavBar from './Components/Navbar/Navigation';
+   import MainPage from './Components/HomePage/MainPage';
+import DisplayPage from './Components/DisplayPage/DisplayPage';
+import PlaceOrder from './Components/PlaceOrder/Placeorder';
+import { BrowserRouter as Router, Route,Routes,Link} from "react-router-dom";
+import CartContextProvider from './Components/CartContext';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          
+             
+             <CartContextProvider>
+             <NavBar />
+              <Routes>  
+              <Route path="" element= {<MainPage />}>
+                    
+                    </Route>          
+                <Route path="/display" element = { <DisplayPage />}>
+                   
+                </Route>
+                <Route path="/order/:id" element = { <PlaceOrder />}>
+                 
+                </Route>
+                <Route path="/checkout" element = { <Checkout />}>
+                 
+                </Route>
+               
+              </Routes>
+              </CartContextProvider>
+      </div>
+    </Router>
   );
 }
+
+
+
 
 export default App;
